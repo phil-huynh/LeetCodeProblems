20c187c4163b1d57007d159dd4ca7ec97a1c6a42
Phil Huynh <philiphuynh98@gmail.com>
2022-01-16 11:45:21 -0500

    valid parentheses updated

--- validParentheses.js
+++ validParentheses.js
@@ -3,5 +3,27 @@
  * @return {boolean}
  */
  var isValid = function(s) {
-
+  if (s.length === 1) {return false}
+  var obj = {
+    '(' : ')',
+    '[' : ']',
+    '{' : '}'
+  }
+  var array = s.split('')
+  var opened = [];
+  var closed = [];
+  for (var i = 0; i < array.length; i ++) {
+    if(array[i] === '(' || array[i] === '[' || array[i] === '{' ){
+      opened.push(array[i]);
+    }
+    if(array[i] === ')' || array[i] === ']' || array[i] === '}') {
+      closed.push(array[i])
+      let lastOpened = opened.pop();
+      if (array[i] !== obj[lastOpened]) {
+        return false;
+      }
+    }
+  }
+  if (opened.length !== 0) {return false}
+  return true;
 };
