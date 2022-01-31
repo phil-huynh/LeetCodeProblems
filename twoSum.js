bf9714bd8576d1c5b04c704f95cfabdde1455a1a
Phil Huynh <philiphuynh98@gmail.com>
2022-01-30 20:11:09 -0500

    updated two sum

--- twoSum.js
+++ twoSum.js
@@ -1,12 +1,10 @@
 var twoSum = function(nums, target) {
   var differences = {}
-  var index1;
-  var index2;
   for (var i = 0; i < nums.length; i++) {
-      if (differences[target - nums[i]] !== undefined) {
-          return [nums.indexOf(target - nums[i]), i]
-      }
-      differences[nums[i]] = target - nums[i];
+    if (differences[nums[i]] !== undefined) {
+      return [differences[nums[i]], i]
+    }
+    differences[target - nums[i]] = i ;
   }
   return null;
 };
